5d4430957b246175ff4790ca530f2bd617d795a2
The CodingFreakj <pallavidapriya75@gmail.com>
2020-07-02 09:49:11 +0530

    API features Added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,85 @@
 const Tour = require('./../models/tourModel');
 
+//Middleware to create an alias route
+
+exports.aliasTopTours = (req, res, next) => {
+  //change the query object for this route
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage, price';
+  req.query.fields = 'name, price, ratingsAverage, summary, difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    const allTours = await Tour.find();
+    //Building the query
+    let destructuredQuery = { ...req.query };
+
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    //Looping and deleting the excluded fields
+    excludedFields.forEach((el) => delete destructuredQuery[el]);
+
+    //console.log(req.query, destructuredQuery);
+
+    //Insert the $expression to the gte variable
+
+    let queryString = JSON.stringify(destructuredQuery);
+
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    //console.log(JSON.parse(queryString));
+
+    let query = Tour.find(JSON.parse(queryString));
+
+    //Sort the API
+
+    if (req.query.sort) {
+      const sortFields = req.query.sort.split(',').join('  ');
+      //console.log(sortFields);
+      query = query.sort(sortFields);
+
+      //sort('price, averageRating') Sort by Price and if they are equal sort them by second field
+    } else {
+      //Default sort by createdAt field
+      query = query.sort('-price');
+    }
+
+    //Fields
+
+    if (req.query.fields) {
+      const selectedFields = req.query.fields.split(',').join('  ');
+      // (" name price duration difficulty")
+      query = query.select(selectedFields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //Pagination
+
+    //Set the default of page and limit
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    //Calculate the skip value
+
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    //If page requested is greater than available
+
+    if (req.query.page) {
+      const totalDocs = await Tour.countDocuments();
+      if (skip >= totalDocs) throw new Error("This page don't exist");
+    }
+
+    //Creating alias route
+
+    const allTours = await query;
 
     res.status(200).json({
       status: 'Success',
